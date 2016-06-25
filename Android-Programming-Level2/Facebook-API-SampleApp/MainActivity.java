3cf091de20ab3476c4126a4937ea1675b111eb61
Jonathan Moreno <JMoreno3821@gmail.com>
2016-06-24 20:49:37 -0500

    Create MainActivity.java

---
+++ Android-Programming-Level2/Facebook-API-SampleApp/MainActivity.java
@@ -0,0 +1,144 @@
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+   // Uri imageuri;
+    AccessTokenTracker accessTokenTracker;
+    //String username, firstname, lastname;
+    TextView profile_name;
+    ImageView profile_pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        profile_name = (TextView) findViewById(R.id.tv_profile_name);
+        profile_pic = (ImageView) findViewById(R.id.iv_profile_pic);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                                try {
+                                    String email = object.getString("email");
+                                    String birthday = object.getString("birthday");
+                                    String id = object.getString("id");
+                                    String name = object.getString("name");
+                                    profile_name.setText(name + "\n" + birthday + "\n" + email);
+
+                                    String imageurl = "https://graph.facebook.com/" + id + "/picture?type=large";
+
+                                    Picasso.with(MainActivity.this).load(imageurl).into(profile_pic);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+/**
+ * AccessTokenTracker to manage logout
+ */
+                accessTokenTracker = new AccessTokenTracker() {
+                    @Override
+                    protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                               AccessToken currentAccessToken) {
+                        if (currentAccessToken == null) {
+                            profile_name.setText("");
+                            profile_pic.setImageResource(0);
+                        }
+                    }
+                };
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+      //  printHahKey();
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+//Generate a Hash Key Via Log.d
+//    void printHahKey(){
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "androidproject.com.androidfacebookapisampleapp",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//
+//               // tv_keyhash.setText(Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
+//    }
+
+
+
+
+}
